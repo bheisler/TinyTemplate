0ec956642701793018becf2a2f8003482ca2bd2b
Andreas Molzer <andreas.molzer@gmx.de>
2022-07-14 19:57:37 +0200

    Fuzz templates with input

--- fuzz/fuzz_targets/render_template.rs
+++ fuzz/fuzz_targets/render_template.rs
@@ -1,12 +1,24 @@
 #![no_main]
+use arbitrary::Arbitrary;
 use libfuzzer_sys::fuzz_target;
+use serde::Serialize;
 
-fuzz_target!(|data: &str| {
+#[derive(Arbitrary, Debug, Serialize)]
+enum Value {
+    Bool(bool),
+    Number(f64),
+    String(String),
+    Array(Vec<Value>),
+    Object(std::collections::HashMap<String, Value>),
+}
+
+fuzz_target!(|val: (&str, Value)| {
+    let (data, value) = val;
     let mut tpl = tinytemplate::TinyTemplate::new();
 
     if tpl.add_template("template", data).is_err() {
         return;
     }
 
-    let _ = tinytemplate::TinyTemplate::new().render("template", &());
+    let _ = tinytemplate::TinyTemplate::new().render("template", &value);
 });
